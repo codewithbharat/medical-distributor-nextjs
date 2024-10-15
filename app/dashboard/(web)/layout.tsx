82d6c283bee3b0754545d3553f89a27efd747284
Bharat Ranjan <ranjanbharat54@gamil.com>
2024-10-15 22:26:20 +0530

    Sidebar Component

--- app/dashboard/(web)/layout.tsx
+++ app/dashboard/(web)/layout.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Sidebar from '@/components/web/Sidebar';
+
 export default function DashboardLayout({
     children,
   }: {
@@ -7,7 +10,7 @@ export default function DashboardLayout({
       <section className="flex flex-col-reverse md:flex-row h-screen w-full dark:bg-[#387478] dark:text-[#E2F1E7] text-[#629584]">
         {/* Sidebar */}
         <section className="dark:bg-[#243642] bg-[#e2f1e7b9] h-12  absolute left-0 right-0 bottom-0 md:static md:w-20 lg:w-56 shadow-sm md:h-screen">
-          sidebar
+          <Sidebar />
         </section>
   
         {/* Main Content */}
